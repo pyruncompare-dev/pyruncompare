b3b7c535e25fdfaeeb2aae070dba7f2089cc8bd6
Tim Gates <tim.gates@iress.com>
2019-07-09 18:40:12 +1000

    Testing tracing;

--- app/pyruncompare/__main__.py
+++ app/pyruncompare/__main__.py
@@ -8,33 +8,77 @@ Usage:
 Options:
     -h --help         Show this screen
     -m <module>       Import and run specified module like python -m
-
 """
-from __future__ import (
-    absolute_import, print_function, division, unicode_literals,
-)
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
-
-# {{{ Imports
 # System Imports
-import sys
+import io
+import linecache
+import os
 import runpy
-# External Imports
-from docopt import docopt
-# }}}
+import sys
+import threading
+
+
+class Tracer(object):
+    """
+    Custom Tracer to record execution
+    """
+    def __init__(self):
+        self.fileobj = io.open('out.txt', 'w', encoding='utf-8')
+
+    def __call__(self, frame, why, arg):  # pylint: disable=unused-argument
+        """Handler for call events."""
+        if why == 'call':
+            filename = frame.f_globals.get('__file__', None)
+            if filename:
+                return self.localtrace
+            else:
+                return None
+        return None
+
+    def localtrace(self, frame, why, arg):  # pylint: disable=unused-argument
+        """
+        Records trace line by line
+        """
+        if why == "line":
+            # record the file name and line number of every trace
+            filename = frame.f_code.co_filename
+            lineno = frame.f_lineno
+            bname = os.path.basename(filename)
+            print(
+                "%s(%d): %s" % (
+                    bname, lineno,
+                    linecache.getline(filename, lineno)
+                ), end='',
+                file=self.fileobj,
+            )
+        return self.localtrace
+
+    def end(self):
+        """
+        Called at completion.
+        """
+        self.fileobj.close()
 
 
 def main():
     """
     Main Command Line entry point
     """
-    args = docopt(__doc__ % {
-        'exename': ''.join(sys.argv[0:1]),
-    })
-    if args.get('-m'):
-        log_module_run(args['-m'], args)
+    args = sys.argv[1:]
+    if len(args) >= 2 and args[0] == '-m':
+        modulename = args[1]
+        log_module_run(modulename, args[2:])
     else:
-        print('Unknown options: %r' % (args,))
+        helptxt = __doc__ % {
+            'exename': ''.join(sys.argv[0:1]),
+        }
+        print(
+            'Unknown options: %r\n\n%s' % (args, helptxt), file=sys.stderr
+        )
+        sys.exit(1)
 
 
 def log_module_run(modulename, args):
@@ -42,9 +86,17 @@ def log_module_run(modulename, args):
     Load and run the module like python -m with log dump enabled.
     """
     oldargs = list(sys.argv)
-    sys.argv = [modulename] + args.get('<args>', [])
-    runpy.run_module(modulename, run_name='__main__', alter_sys=True)
-    sys.argv = oldargs
+    sys.argv = [modulename] + args
+    tracer = Tracer()
+    sys.settrace(tracer)
+    threading.settrace(tracer)
+    try:
+        runpy.run_module(modulename, run_name='__main__', alter_sys=True)
+    finally:
+        tracer.end()
+        sys.settrace(None)
+        threading.settrace(None)
+        sys.argv = oldargs
 
 
 if __name__ == '__main__':
--- app/setup.py
+++ app/setup.py
@@ -1,22 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Setuptool Distribution for pyruncompare
+"""
+
+# {{{ Import
+# System  Imports
+import codecs
+import os
+
+# External Imports
 from setuptools import setup
+
+# }}}
+
+
+def read(fname):
+    """
+    Read the contents of relative file.
+    """
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
 
 setup(
     name='pyruncompare',
     version='0.1.1dev',
     author='Tim Gates',
     author_email='tim.gates@iress.com',
-    packages=['pyruncompare',],
+    maintainer='Tim Gates',
+    maintainer_email='tim.gates@iress.com',
+    packages=['pyruncompare'],
     license='GPLv3+',
-    long_description=(
+    description=(
         'Execute a python module or function and log all'
         ' calls and locals to formats that can be compared'
         ' for execution variations.'
     ),
+    long_description=read('README.md'),
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    install_requires=[],
     url='https://github.com/pyruncompare-dev/pyruncompare',
-    packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE V3 (GPLV3)",
-        "Operating System :: OS Independent",
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     ],
 )
